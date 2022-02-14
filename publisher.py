2fe7e71047e77b54c3b68786968ad2368a8098b0
Mert Cobanov <mertcobanov@gmail.com>
2022-02-14 16:10:23 +0300

    rpi update

---
+++ publisher.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+from random import randrange, uniform
+import time
+import time
+import board
+import adafruit_dht
+
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D17)
+mqttBroker = "test.mosquitto.org"
+
+client = mqtt.Client("Temperature_Inside")
+client.connect(mqttBroker)
+
+while True:
+    try:
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        payload = (
+            f"Temp: {temperature_f} F / {temperature_c} C    Humidity: {humidity} "
+        )
+        client.publish("Cobanov", payload)
+        print(payload)
+
+    except RuntimeError as error:  # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+
+    time.sleep(1.0)
